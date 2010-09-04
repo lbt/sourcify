d5e19e859f72d54b81178bb425d75189be36b57b
NgTzeYang <ngty77@gmail.com>
2010-09-04 20:46:42 +0800

    Adhoc specs are relocated to adhoc_scanner_spec.rb.

---
+++ lib/sourcify/proc/adhoc_scanner_spec.rb
@@ -0,0 +1,287 @@
+# NOTE: This spec file is only intended for ad-hoc development of ./scanner.rl.
+require 'rubygems'
+require 'bacon'
+Bacon.summary_on_exit
+
+module Sourcify::Proc::Scanner
+  class << self ; attr_reader :tokens ; end
+end
+
+def process(data)
+  Sourcify::Proc::Scanner.process(data)
+  Sourcify::Proc::Scanner.tokens
+end
+
+describe 'Single quote strings' do
+
+  %w{~ ` ! @ # $ % ^ & * _ - + = \\ | ; : ' " , . ? /}.map{|w| [w,w] }.concat(
+    [%w{( )}, %w{[ ]}, %w({ }), %w{< >}]
+  ).each do |q1,q2|
+    %w{q w}.each do |t|
+
+      should "handle '%#{t}#{q1}...#{q2}' (wo escape)" do
+        process(<<-EOL
+          %#{t}#{q1}hello#{q2}
+EOL
+        ).should.include((<<-EOL
+          %#{t}#{q1}hello#{q2}
+EOL
+        ).strip)
+      end
+
+#        should "handle '%#{t}#{q1}...#{q2}' (w escape)" do
+#          process(<<-EOL
+#            %#{t}#{q1}hel\\#{q2}lo#{q2}
+#EOL
+#          ).should.include((<<-EOL
+#            %#{t}#{q1}hel\\#{q2}lo#{q2}
+#EOL
+#          ).strip)
+#        end
+
+      should "handle '%#{t}#{q1}...#{q2}' (wo escape & multiple)" do
+        process(<<-EOL
+          %#{t}#{q1}hello#{q2} %#{t}#{q1}world#{q2}
+EOL
+        ).should.include((<<-EOL
+          %#{t}#{q1}hello#{q2}
+EOL
+        ).strip)
+      end
+
+    end
+  end
+
+  should "handle '...' (wo escape)" do
+    process(<<-EOL
+      'hello'
+EOL
+    ).should.include((<<-EOL
+      'hello'
+EOL
+    ).strip)
+  end
+
+#    should "handle '...' (w escape)" do
+#      process(<<-EOL
+#        'hel\'lo'
+#EOL
+#      ).should.include((<<-EOL
+#        'hel\'lo'
+#EOL
+#      ).strip)
+#    end
+
+  should "handle '...' (wo escape & multiple)" do
+    process(<<-EOL
+      'hello' 'world'
+EOL
+    ).should.include((<<-EOL
+      'hello'
+EOL
+    ).strip)
+  end
+
+end
+
+describe 'Double quote strings (wo interpolation)' do
+
+  %w{~ ` ! @ # $ % ^ & * _ - + = \\ | ; : ' " , . ? /}.map{|w| [w,w] }.concat(
+    [%w{( )}, %w{[ ]}, %w({ }), %w{< >}]
+  ).each do |q1,q2|
+    %w{Q W x r}.each do |t|
+
+      should "handle '%#{t}#{q1}...#{q2}' (wo escape)" do
+        process(<<-EOL
+          %#{t}#{q1}hello#{q2}
+EOL
+        ).should.include((<<-EOL
+          %#{t}#{q1}hello#{q2}
+EOL
+        ).strip)
+      end
+
+#        should "handle '%#{t}#{q1}...#{q2}' (w escape)" do
+#          process(<<-EOL
+#            %#{t}#{q1}hel\\#{q2}lo#{q2}
+#EOL
+#          ).should.include((<<-EOL
+#            %#{t}#{q1}hel\\#{q2}lo#{q2}
+#EOL
+#          ).strip)
+#        end
+
+      should "handle '%#{t}#{q1}...#{q2}' (wo escape & multiple)" do
+        process(<<-EOL
+          %#{t}#{q1}hello#{q2} %#{t}#{q1}world#{q2}
+EOL
+        ).should.include((<<-EOL
+          %#{t}#{q1}hello#{q2}
+EOL
+        ).strip)
+      end
+
+    end
+  end
+
+  %w{" / `}.each do |q|
+
+    should 'handle #{q}...#{q} (wo escape)' do
+      process(<<-EOL
+        #{q}hello#{q}
+EOL
+      ).should.include((<<-EOL
+        #{q}hello#{q}
+EOL
+      ).strip)
+    end
+
+    should 'handle #{q}...#{q} (wo escape & multiple)' do
+      process(<<-EOL
+        #{q}hello#{q} #{q}world#{q}
+EOL
+      ).should.include((<<-EOL
+        #{q}hello#{q}
+EOL
+      ).strip)
+    end
+
+#      should 'handle #{q}...#{q} (w escape)' do
+#        process(<<-EOL
+#          #{q}hel\#{q}lo#{q}
+#EOL
+#        ).should.include((<<-EOL
+#          #{q}hel\#{q}lo#{q}
+#EOL
+#        ).strip)
+#      end
+
+  end
+
+end
+
+describe 'Commented lines' do
+
+  should 'handle # ...' do
+    process(<<EOL
+      hello # blah
+      world
+EOL
+    ).should.include("# blah\n")
+  end
+
+  should 'handle =begin ... =end' do
+    process(<<EOL
+      hello
+=begin aa
+bb
+=end cc
+      world
+EOL
+      ).should.include("\n=begin aa\nbb\n=end cc\n")
+  end
+
+end
+
+describe 'Heredoc strings' do
+
+  should 'handle <<-X\n .. \nX\n' do
+    process(<<EOL
+      aa
+      s <<-X
+        bb 
+X
+      cc
+EOL
+    ).should.include("<<-X\n          bb \nX\n")
+  end
+
+  should 'not handle <<-X\n .. \nX \n' do
+    process(<<EOL
+      aa
+      s <<-X
+        bb
+X 
+      cc
+EOL
+    ).should.not.include("<<-X\n          bb \nX \n")
+  end
+
+  should 'handle <<-X\n .. \n  X\n' do
+    process(<<EOL
+      aa
+      s <<-X
+        bb 
+X
+      cc
+EOL
+    ).should.include("<<-X\n          bb \n  X\n")
+  end
+
+  should 'not handle <<-X\n .. \n  X \n' do
+    process(<<EOL
+      aa
+      s <<-X
+        bb 
+X 
+      cc
+EOL
+    ).should.not.include("<<-X\n          bb \n  X \n")
+  end
+
+  should 'handle <<X\n .. \nX' do
+    process(<<EOL
+      aa
+      s <<X
+        bb 
+X
+      cc
+EOL
+    ).should.include("<<X\n          bb \nX\n")
+  end
+
+  should 'not handle <<X\n .. \nX ' do
+    process(<<EOL
+      aa
+      s <<X
+        bb 
+X 
+      cc
+EOL
+    ).should.not.include("<<X\n          bb \nX \n")
+  end
+
+  should 'not handle <<X\n .. \n  X' do
+    process(<<EOL
+      aa
+      s <<X
+        bb 
+X
+      cc
+EOL
+    ).should.not.include("<<X\n          bb \n  X\n")
+  end
+
+  should 'not handle class <<X ..' do
+    process(<<EOL
+      aa
+      class <<X
+        bb 
+X
+      cc
+EOL
+    ).should.not.include("<<X\n          bb \nX\n")
+  end
+
+  should 'handle xclass <<X ..' do
+    process(<<EOL
+      aa
+      xclass <<X
+        bb 
+X
+      cc
+EOL
+    ).should.include("<<X\n          bb \nX\n")
+  end
+
+end
