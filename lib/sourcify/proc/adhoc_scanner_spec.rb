9012cedfddd24ba87c3d4019cc219482a7dc0561
NgTzeYang <ngty77@gmail.com>
2010-09-05 09:32:44 +0800

    Preparation for unexpected failures in sourcify/proc specs.

--- lib/sourcify/proc/adhoc_scanner_spec.rb
+++ lib/sourcify/proc/adhoc_scanner_spec.rb
@@ -35,42 +35,42 @@ describe 'Single quote strings' do
     %w{q w}.each do |t|
 
       should "handle '%#{t}#{q1}...#{q2}' (wo escape (single))" do
-        process(" %#{t}#{q1}hello#{q2} ").should.include("%#{t}#{q1}hello#{q2}")
+        process(" xx %#{t}#{q1}hello#{q2} ").should.include("%#{t}#{q1}hello#{q2}")
       end
 
       should "handle '%#{t}#{q1}...#{q2}' (wo escape (multiple))" do
-        tokens = process(" %#{t}#{q1}hello#{q2} %#{t}#{q1}world#{q2} ")
+        tokens = process(" xx %#{t}#{q1}hello#{q2} %#{t}#{q1}world#{q2} ")
         tokens.should.include("%#{t}#{q1}hello#{q2}")
         tokens.should.include("%#{t}#{q1}world#{q2}")
       end
 
       should "handle '%#{t}#{q1}...#{q2}' (w escape (single))" do
-        process(" %#{t}#{q1}hel\\#{q2}lo#{q2} ").should.include("%#{t}#{q1}hel\\#{q2}lo#{q2}")
+        process(" xx %#{t}#{q1}hel\\#{q2}lo#{q2} ").should.include("%#{t}#{q1}hel\\#{q2}lo#{q2}")
       end
 
       should "handle '%#{t}#{q1}...#{q2}' (w escape (multiple))" do
-        process(" %#{t}#{q1}h\\#{q2}el\\#{q2}lo#{q2} ").should.include("%#{t}#{q1}h\\#{q2}el\\#{q2}lo#{q2}")
+        process(" xx %#{t}#{q1}h\\#{q2}el\\#{q2}lo#{q2} ").should.include("%#{t}#{q1}h\\#{q2}el\\#{q2}lo#{q2}")
       end
 
     end
   end
 
   should "handle '...' (wo escape (single))" do
-    process(" 'hello' ").should.include("'hello'")
+    process(" xx 'hello' ").should.include("'hello'")
   end
 
   should "handle '...' (wo escape (multiple))" do
-    tokens = process(" 'hello' 'world' ")
+    tokens = process(" xx 'hello' 'world' ")
     tokens.should.include("'hello'")
     tokens.should.include("'world'")
   end
 
   should "handle '...' (w escape (single))" do
-    process(" 'hel\\'lo' ").should.include("'hel\\'lo'")
+    process(" xx 'hel\\'lo' ").should.include("'hel\\'lo'")
   end
 
   should "handle '...' (w escape (multiple))" do
-    process(" 'h\\'el\\'lo' ").should.include("'h\\'el\\'lo'")
+    process(" xx 'h\\'el\\'lo' ").should.include("'h\\'el\\'lo'")
   end
 
 end
@@ -83,21 +83,21 @@ describe 'Double quote strings (wo interpolation)' do
     %w{Q W x r}.each do |t|
 
       should "handle '%#{t}#{q1}...#{q2}' (wo escape (single))" do
-        process("  %#{t}#{q1}hello#{q2}  ").should.include("%#{t}#{q1}hello#{q2}")
+        process(" xx %#{t}#{q1}hello#{q2} ").should.include("%#{t}#{q1}hello#{q2}")
       end
 
       should "handle '%#{t}#{q1}...#{q2}' (wo escape (multiple))" do
-        tokens = process("  %#{t}#{q1}hello#{q2} %#{t}#{q1}world#{q2}  ")
+        tokens = process(" xx  %#{t}#{q1}hello#{q2} %#{t}#{q1}world#{q2}  ")
         tokens.should.include("%#{t}#{q1}hello#{q2}")
         tokens.should.include("%#{t}#{q1}world#{q2}")
       end
 
       should "handle '%#{t}#{q1}...#{q2}' (w escape (single))" do
-        process(" %#{t}#{q1}hel\\#{q2}lo#{q2} ").should.include("%#{t}#{q1}hel\\#{q2}lo#{q2}")
+        process(" xx %#{t}#{q1}hel\\#{q2}lo#{q2} ").should.include("%#{t}#{q1}hel\\#{q2}lo#{q2}")
       end
 
       should "handle '%#{t}#{q1}...#{q2}' (w escape (multiple))" do
-        process(" %#{t}#{q1}h\\#{q2}el\\#{q2}lo#{q2} ").should.include("%#{t}#{q1}h\\#{q2}el\\#{q2}lo#{q2}")
+        process(" xx %#{t}#{q1}h\\#{q2}el\\#{q2}lo#{q2} ").should.include("%#{t}#{q1}h\\#{q2}el\\#{q2}lo#{q2}")
       end
 
     end
@@ -106,21 +106,21 @@ describe 'Double quote strings (wo interpolation)' do
   %w{" / `}.each do |q|
 
     should 'handle #{q}...#{q} (wo escape (single))' do
-      process(%Q( #{q}hello#{q} )).should.include(%Q(#{q}hello#{q}))
+      process(%Q( xx #{q}hello#{q} )).should.include(%Q(#{q}hello#{q}))
     end
 
     should 'handle #{q}...#{q} (wo escape & multiple)' do
-      tokens = process(%Q( #{q}hello#{q} #{q}world#{q} ))
+      tokens = process(%Q( xx #{q}hello#{q} #{q}world#{q} ))
       tokens.should.include(%Q(#{q}hello#{q}))
       tokens.should.include(%Q(#{q}world#{q}))
     end
 
     should 'handle #{q}...#{q} (w escape (single))' do
-      process(%Q( #{q}hel\\#{q}lo#{q} )).should.include(%Q(#{q}hel\\#{q}lo#{q}))
+      process(%Q( xx #{q}hel\\#{q}lo#{q} )).should.include(%Q(#{q}hel\\#{q}lo#{q}))
     end
 
     should 'handle #{q}...#{q} (w escape (multiple))' do
-      process(%Q( #{q}h\\#{q}el\\#{q}lo#{q} )).should.include(%Q(#{q}h\\#{q}el\\#{q}lo#{q}))
+      process(%Q( xx #{q}h\\#{q}el\\#{q}lo#{q} )).should.include(%Q(#{q}h\\#{q}el\\#{q}lo#{q}))
     end
 
   end
